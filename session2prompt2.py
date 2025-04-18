2758ed24d60ead9a7de86f619ad377589c9a2831
zfsayed <zfsayed@ucsc.edu>
2025-04-18 13:43:19 -0700

    Adding Code Journal 1 Prompt 2

---
+++ session2prompt2.py
@@ -0,0 +1,9 @@
+# session2prompt2.py
+
+a = 5.2 + 3.1
+b = 10 - 3
+c = 2.5 * 4
+
+print("Sum:", a, "Type:", type(a))
+print("Difference:", b, "Type:", type(b))
+print("Product:", c, "Type:", type(c))
