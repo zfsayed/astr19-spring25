96a0dc3ded40a5294c70e25f403ee22de058c3a2
zfsayed <zfsayed@ucsc.edu>
2025-04-18 13:38:55 -0700

    Add files via upload

---
+++ session1prompt1.py
@@ -0,0 +1,6 @@
+# session1prompt1.py
+
+print("Zara Sayed")
+print("Pronouns: she/her")
+print("My favorite movie is Interstellar because it blends science and emotion so well.")
+print("My favorite food is biryani, nothing beats the spice!")
